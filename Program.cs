5de5b2edf107c612091f4ed6c471a481610b6068
pbaekgaard <peterbaekgaard.studie@gmail.com>
2022-10-14 11:41:54 +0200

    Peters shit

--- Program.cs
+++ Program.cs
@@ -1,5 +1,33 @@
 ﻿namespace sixthLec
 {
+    public class Pair<T1,T2>
+    {
+        public Pair(T1 val1, T2 val2)
+        {
+            value1 = val1;
+            value2 = val2;
+        }
+        public T1 value1 { get;  }
+        public T2 value2 { get; }
+
+
+
+        public (T2, T1) swap()
+        {
+            return (value2, value1);
+        }
+
+        public (C, T2) setFst<C>(C val)
+        {
+            return (val, value2);
+        }
+
+        public (T1, C) setSnd<C>(C val)
+        {
+            return (value1, val);
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -13,6 +41,21 @@
 
             }
             #endregion
+
+            #region Opgave 2
+                void opg2()
+                {
+                    Pair<int, bool> myPair = new Pair<int, bool>(14, true);
+                    Console.WriteLine($"{myPair.swap()}");
+                    Console.WriteLine($"{myPair.setFst<string>("Henro")}");
+                    Console.WriteLine($"{myPair.setSnd<double>(37.98)}");
+                }
+
+
+            opg2();
+
+            #endregion
         }
+
     }
 }
