7dc02361a1344f043b3964db59b58ac425a6c233
Mcklmo <87199710+Mcklmo@users.noreply.github.com>
2022-10-21 09:38:15 +0200

    .

--- Pair.cs
+++ Pair.cs
@@ -9,23 +9,17 @@
         }
         public T1 value1 { get;  }
         public T2 value2 { get; }
-
-
-
         public (T2, T1) swap()
         {
             return (value2, value1);
         }
-
         public (C, T2) setFst<C>(C val)
         {
             return (val, value2);
         }
-
         public (T1, C) setSnd<C>(C val)
         {
             return (value1, val);
         }
-
     }
 }
